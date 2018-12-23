bf2536a98639b863cf0c661d1ba2b1c2f108c80e
seemal <schauhan1206@gmail.com>
2018-12-23 15:38:29 -0500

    commit and push data

---
+++ midterm2018/src/projectsunday/DataStructureQuestions.java
@@ -0,0 +1,45 @@
+package projectsunday;
+
+public class DataStructureQuestions {
+	Please answer the below Data Structure Questions:
+
+		why do we need data structure? Data structure is a particular way of organizing and storing data in a computer so that it can be accessed and modified efficiently
+		A.
+		What are the various operations that can be performed on different Data Structures? Operations on Data Structures. The basic operations that are performed on data structures are as follows: Insertion: Insertion means addition of a new data element in a data structure. Deletion: Deletion means removal of a data element from a data structure if it is found
+		List out the areas in which data structures are applied extensively?Compiler Design,Operating System,Database Management System, Statistical analysis package,Numerical Analysis,Graphics,Artificial Intelligence,Simulation
+		what is an Array? An array, in the context of Java, is a dynamically created object that serves as a container to hold constant number of values of the same type
+		what is the difference between the Array and ArrayList?  First and Major difference between Array and ArrayList in Java is that Array is a fixed length data structure while ArrayList is a variable length Collection class
+		what is LinkedList? Java LinkedList class uses a doubly linked list to store the elements
+		How is an Array different from Linked List? In array, each element is independent and can be accessed using it's index value. In case of a linked list, each node/element points to the next, previous, or maybe both nodes
+		what is queue? Java Queue is an interface available in java.util package and extends java.util.Collection interface
+		what is stack?Java Stack memory is used for execution of a thread
+		what is FIFO and LIFO? Java collection framework provides different implementations of Queue, there are queues which order elements in first in first out(FIFO), last in first out (LIFO), in natural order or using the provided comparator
+		what is the order of complexity? This means that it is a certain mathematical expression of the size of the input, and the algorithm finishes between two factors of it
+		what is the best case to search an element from an array? Heap sort Smooth sort	
+		what is the worst case to search an element from an array?
+		what is tree in data structure? A tree is a nonlinear data structure
+		what is graph in data structure? A Graph is a non-linear data structure consisting of nodes and edges
+		what is the difference between the HashTable and HashMap?  Hashtable is synchronized, whereas HashMap is not
+		What are the major data structures used in the following areas : RDBMS, Network data model and Hierarchical data model.
+		How HashMap works in java? HashMap in Java works on hashing principle. It is a data structure which allows us to store object and retrieve it in constant time O(1) provided we know the key
+				
+		What is ArrayIndexOutOfBoundsException in java? When it occurs? java.lang.ArrayIndexOutOfBoundsException. ArrayIndexOutOfBoundsException is thrown to indicate that we are trying to access array element with an illegal index
+		What are the different ways of copying an array into another array?  Manually Arrays.copyOf() 3. System.arraycopy() 4. Object.clone()
+
+		What is difference between an array and a linked list? Difference between Array and Linked List? Basically, an array is a set of similar data objects stored in sequential memory locations under a common heading or a variable name
+		What is DFS and BFS?DFS is a recursive algorithm whereas BFS is an iterative one and is implemented using a queue
+		What is Recursion? Recursion is a basic programming technique you can use in Java, in which a method calls itself to solve some problem
+		What are linear and non linear data Structures? Linear data structure: A linear data structure traverses the data elements sequentially, in which only one data element can directly be reached
+		What is Big-(O)-notation? A beginner's guide to Big O notation. Big O notation is used in Computer Science to describe the performance or complexity of an algorithm 
+		Outlined different kind of sorting algorithm with time complexity? Bubble Sort Insertion Sort	Heap Sort	
+
+		Draw a table with time complexity in which it will summarize all the data structure feature's advantages and disadvantages.
+		
+		
+		  a)In terms of Accessing element.
+		  b)In terms of Inserting element.
+		  c)In terms of Searching element.
+		  d)In terms of Removing element.
+}
+
+
