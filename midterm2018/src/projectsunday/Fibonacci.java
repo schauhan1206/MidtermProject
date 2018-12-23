854928b315fd6bb53ea4a43a894231da902ad39b
seemal <schauhan1206@gmail.com>
2018-12-23 18:45:35 -0500

    fibonacci commitnd push

---
+++ midterm2018/src/projectsunday/Fibonacci.java
@@ -0,0 +1,25 @@
+package projectsunday;
+
+public class Fibonacci {
+	
+	public static void main (String [] args) {
+		 int n, a = 0, b = 0, c = 1;
+	        System.out.print("Fibonacci Series:");
+	        for(int i = 1; i <= 50; i++)
+	        {
+	            a = b;
+	            b = c;
+	            c = a + b;
+	            System.out.print(a+" ");
+	        }
+	}
+		
+	}
+		
+	
+		 
+	     
+
+	
+
+
