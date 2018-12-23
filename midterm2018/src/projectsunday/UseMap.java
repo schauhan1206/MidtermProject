65889603d4e360c045028f2b122d69a39de8e0e9
seemal <schauhan1206@gmail.com>
2018-12-23 16:02:27 -0500

    commit and push map

---
+++ midterm2018/src/projectsunday/UseMap.java
@@ -0,0 +1,24 @@
+package projectsunday;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UseMap {
+
+	public static void main(String[] args) {
+	      Map m1 = new HashMap();
+	      m1.put("Seemal", "8");
+	      m1.put("Nisha", "22");
+	      m1.put("Aamar", "20");
+	      m1.put("Pnt", "10");
+	      System.out.println();
+	      System.out.println(" Map Elements");
+	      System.out.print("\t" + m1);
+
+	      
+
+
+
+	}
+
+}
