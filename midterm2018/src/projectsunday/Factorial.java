ff3fcbbee082452014c9e9e9ecda8f99e75f9328
seemal <schauhan1206@gmail.com>
2018-12-25 18:22:34 -0500

    commit and push factorial number

---
+++ midterm2018/src/projectsunday/Factorial.java
@@ -0,0 +1,26 @@
+package projectsunday;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+	 int number =5;
+	 int factorial = number;
+	 for (int i = (number -1); i > 1; i--) {
+		 factorial = factorial *i;
+		 
+	 }
+	
+	System.out.println("factorial of 5 is " + factorial);
+	
+	 
+			 
+	 
+	 
+	 
+			 
+			 
+			 
+
+	}
+
+}
